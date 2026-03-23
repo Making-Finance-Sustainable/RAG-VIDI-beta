b85f34c32a168df55a0f32eea63258fda149366f
Bastián González-Bustamante <b.a.gonzalez.bustamante@fgga.leidenuniv.nl>
2026-03-23 17:39:58 +0100

    Update stage_2_boxplots_region_assets.py

--- code/stage_2_boxplots_region_assets.py
+++ code/stage_2_boxplots_region_assets.py
@@ -19,7 +19,8 @@ np.random.seed(42)
 ##########################################################################
 
 ## Data
-csv_path = "data/raw/reports_topics.csv"
+## csv_path = "data/raw/reports_topics.csv"
+csv_path = "data/tidy/reports_topics_validation.csv"
 
 df = pd.read_csv(csv_path)
 
